43647098cd413db8c26ab829850413ac22e9a385
Carlos Fabian <carl.fab.g@gmail.com>
2021-02-05 14:29:30 -0600

    set up

---
+++ src/main/java/com/beermarketplace/BeerMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.beermarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerMarketPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerMarketPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beermarketplace/BeerMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beermarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerMarketPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
